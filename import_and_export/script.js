0e4592ffa9d117c9bac3d78f9c2d422128219a65
TANISH SHARMA <101451522+tanishs874@users.noreply.github.com>
2024-04-28 08:15:11 +0000

    import and export in js

---
+++ import_and_export/script.js
@@ -0,0 +1,13 @@
+var data = require('./script2')
+
+var a = 1
+
+var b = 2
+
+var c = 3
+
+console.log(data.first,data.second);
+
+// Node js hai khoob sara c++ ka code ji ki liya gaya hai chrome browser ke v8 engine ke code se aur ye c++ code jsiko liya gaya hai wrap kar diya 
+// gaya hai js code se and ye hume allow karta hai hum js mai code likhe aur ye js code accept karega and c++ ke basis par apko server environment
+// bana kar dega par sirf tab tak jab tak ye code chlaega jab tak node(v8 engine and js wrapper) chalta hai..
---
+++ import_and_export/script2.js
@@ -0,0 +1,8 @@
+var d = 12345;
+// module.exports = d;//exporting the value of d from this file to another file
+
+// How to export more than 1 file 
+
+var tanish = 12
+var sharma = 13
+module.exports =  {first:tanish, second:sharma}
