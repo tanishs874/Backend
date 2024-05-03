fde0988d8fc824d8db12c41d6442516369e02b8a
TANISH SHARMA <101451522+tanishs874@users.noreply.github.com>
2024-05-03 05:52:57 +0000

    session

---
+++ session and cookies /session.js
@@ -0,0 +1,33 @@
+// session ---> Server par data save hota hai 
+// install session 
+// ===> npm i express-session
+//---> Now in main file app.js
+// var session = require("express-session")
+
+// app.use(session({
+//     resave:false,
+//     saveUninitialized:false,
+//     secret:"holabholaholabhola"
+// }))
+
+
+// ---> indexe.js file 
+
+// router.get("/",function(req,res){
+//     req.session.ban = true;
+//     res.render("index");
+//   })
+  
+//   router.get("/checkban",function(req,res){
+//     if(req.session.ban===true){
+//       res.send("YOU ARE BANNED !!!");
+//     }
+//   });
+  
+//   // How to remove the ban
+//   router.get("/removeban",function(req,res){
+//     req.session.destroy(function(err){
+//       if(err) throw err;
+//       res.send("BAN REMOVED");
+//     })
+//   })
